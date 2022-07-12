fd7c12cc3b02232f26c73f244e99e36c3653f9e7
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2022-07-12 13:43:56 +0300

    add todos slice

---
+++ src/Todos/todosSlice.ts
@@ -0,0 +1,27 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {RootState} from '../app/reducer';
+
+interface Todo {
+    readonly id: number | string;
+    readonly title: string;
+    readonly description: string;
+    readonly createdAt: string;
+    readonly completed: boolean;
+}
+
+const initialState: readonly Todo[] = [];
+
+const slice = createSlice({
+    name: 'todos',
+    initialState,
+    reducers: {
+        add: (state, action) => {
+            console.log(state);
+            console.log(action);
+        }
+    }
+});
+
+export const { reducer: todosReducer, actions: todosActions } = slice;
+
+export const selectTodos = (state: RootState) => state.todos;
--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -1,5 +1,8 @@
 import {combineReducers} from 'redux';
+import {todosReducer} from '../Todos/todosSlice';
 
-export const reducer = combineReducers({});
+export const reducer = combineReducers({
+    todos: todosReducer
+});
 
 export type RootState = ReturnType<typeof reducer>;
