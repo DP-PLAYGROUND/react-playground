12fee4f3f4b6dc2a1b440339f244ab3f983fdef2
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2022-07-15 17:51:18 +0300

    update todos api

--- src/api/Todos/TodosAPI.ts
+++ src/api/Todos/TodosAPI.ts
@@ -1,22 +1,29 @@
 import {Todo, TodoDraft} from './Todo';
 import {JSONPlaceholderProvider} from '../JSONPlaceholderProvider';
 
+const provider = JSONPlaceholderProvider;
+
 const path = 'todos';
 
 const getAll = () => {
-    return JSONPlaceholderProvider.get<readonly Todo[]>(path).then(res => res.data);
+    return provider.get<readonly Todo[]>(path).then(res => res.data);
 }
 
 const create = (todo: TodoDraft) => {
-    return JSONPlaceholderProvider.post<Todo>(path, todo).then(res => res.data);
+    return provider.post<Todo>(path, todo).then(res => res.data);
 }
 
 const update = (id: Todo['id'], fields: Partial<TodoDraft>) => {
-    return JSONPlaceholderProvider.patch<Todo>(`${path}/${id}`, fields).then(res => res.data);
+    return provider.patch<Todo>(`${path}/${id}`, fields).then(res => res.data);
+}
+
+const remove = (id: Todo['id']) => {
+    return provider.delete(`${path}/${id}`).then(res => res.data);
 }
 
 export const TodosApi = {
     getAll,
     create,
-    update
+    update,
+    remove
 }
