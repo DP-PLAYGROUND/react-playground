cd547c374f285972e4c7783740e7e96947a2d532
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2022-07-16 16:00:48 +0300

    load todos on page load

--- src/Todos/Todos.tsx
+++ src/Todos/Todos.tsx
@@ -1,11 +1,21 @@
-import {FunctionComponent} from 'react';
+import {FunctionComponent, useEffect} from 'react';
 import styles from './Todos.module.scss'
 import {TodosList} from './TodosList/TodosList';
+import {useAppDispatch, useAppSelector} from '../app/hooks';
+import {loadTodos, selectAllTodos} from './todosSlice';
 
 const Todos: FunctionComponent = () => {
+    const todos = useAppSelector(selectAllTodos);
+
+    const appDispatch = useAppDispatch();
+
+    useEffect(() => {
+        appDispatch(loadTodos())
+    }, [appDispatch]);
+
     return (
         <section className={styles.todos}>
-            <TodosList todos={[]}/>
+            <TodosList todos={todos}/>
         </section>
     )
 }
