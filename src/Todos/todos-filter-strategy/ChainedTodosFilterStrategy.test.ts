af63e8526e28c9d669ad7fc2687a74f366fddd50
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2022-07-18 13:16:45 +0300

    test for ChainedTodosFilterStrategy

---
+++ src/Todos/todos-filter-strategy/ChainedTodosFilterStrategy.test.ts
@@ -0,0 +1,45 @@
+import {ChainedTodosFilterStrategy} from './ChainedTodosFilterStrategy';
+import {QueryTodosFilterStrategy} from './QueryTodosFilterStrategy';
+import {StatusTodosFilterStrategy} from './StatusTodosFilterStrategy';
+import {TodosFilterStatusType} from '../todos-filter-params/TodosFilterStatusType';
+import {SortTodosFilterStrategy} from './SortTodosFilterStrategy';
+import {TodosFilterSortType} from '../todos-filter-params/TodosFilterSortType';
+import {Todo} from '../Todo';
+import SpyInstance = jest.SpyInstance;
+
+describe('ChainedTodosFilterStrategy', () => {
+    let chainedStrategy: ChainedTodosFilterStrategy;
+    let executeSpies: SpyInstance<readonly Todo[], [todos: readonly Todo[]]>[];
+
+    beforeEach(() => {
+        const strategies = [
+            new QueryTodosFilterStrategy(''),
+            new StatusTodosFilterStrategy(TodosFilterStatusType.all),
+            new SortTodosFilterStrategy(TodosFilterSortType.name)
+        ];
+
+        executeSpies = strategies.map(strategy => jest.spyOn(strategy, 'execute'))
+
+        chainedStrategy = new ChainedTodosFilterStrategy(strategies);
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should execute strategies', () => {
+        chainedStrategy.execute([]);
+
+        executeSpies.forEach(spy => expect(spy).toHaveBeenCalled())
+    });
+
+    it('should follow execution order', () => {
+        chainedStrategy.execute([]);
+
+        executeSpies.reduce((acc, curr) => {
+            const invocationCall = curr.mock.invocationCallOrder[0];
+            expect(invocationCall).toBeGreaterThan(acc);
+            return invocationCall;
+        }, 0)
+    });
+})
