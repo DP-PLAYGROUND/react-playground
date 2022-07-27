4261b362f359683bcb061303618ab000fbdde7b4
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2022-07-27 22:47:21 +0300

    remove redundant styles from drawing palette

--- src/Drawing/Drawing.tsx
+++ src/Drawing/Drawing.tsx
@@ -1,13 +1,12 @@
 import { FunctionComponent } from "react";
 import { DrawingPalette } from "./DrawingPalette/DrawingPalette";
-import styles from "./Drawing.module.scss";
 import { FreeHandDrawing } from "./DrawingPalette/FreeHandDrawing/FreeHandDrawing";
 import { DrawingExport } from "./DrawingPalette/DrawingExport/DrawingExport";
 import { DrawingUndoRedo } from "./DrawingPalette/DrawingUndoRedo/DrawingUndoRedo";
 
 const Drawing: FunctionComponent = () => {
   return (
-    <DrawingPalette className={styles.palette}>
+    <DrawingPalette>
       <FreeHandDrawing />
       <DrawingUndoRedo/>
       <DrawingExport type="image/webp" quality={1}/>
