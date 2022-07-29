9c32733ab85148d67cf91436e2eba4b857bd3f93
Dmytro Parfenov <dmitryparfenov937@gmail.com>
2022-07-29 23:48:15 +0300

    simplify user form

--- src/UserForm/UserForm.tsx
+++ src/UserForm/UserForm.tsx
@@ -1,24 +1,23 @@
 import { Field, Form, Formik } from "formik";
 import { FunctionComponent } from "react";
-import { User } from "./User";
-import styles from './UserForm.module.scss';
-
-const initialValues: User = {
-  name: "",
-  email: ""
-};
+import styles from "./UserForm.module.scss";
 
 const UserForm: FunctionComponent = () => {
-  const handleSubmit = (data: typeof initialValues) => {
-    console.log(data);
-  };
-
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        password: ""
+      }}
+      onSubmit={values => {
+        console.log(values);
+      }}
+    >
       <Form className={styles.form}>
         <Field name="name" placeholder="Name"></Field>
         <Field name="email" type="email" placeholder="Email"></Field>
-        <Field name="age" type="number" placeholder="Age"></Field>
+        <Field name="password" type="password" placeholder="Password"></Field>
         <button type="submit">Submit</button>
       </Form>
     </Formik>
